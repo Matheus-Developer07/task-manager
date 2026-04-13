5124096b3e97607c5425efa851398435f51ba0ca
Ghost007Java <matheusfelipeferreirawork@gmail.com>
2026-04-13 18:21:58 -0300

    I finished the project by adding a remove button and a checkbox.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const input = document.querySelector("#taskInput");
 
 function addElement(){
     const task = document.createElement("h2");
+    task.classList.add("task")
     const text = input.value.trim();
     if(text === ""){
         alert("Você precisa digitar uma tarefa!");
@@ -13,6 +14,29 @@ function addElement(){
     tasks.appendChild(task);
 
     input.value = "";
+
+// Check box
+    const checkBox = document.createElement("input")
+    checkBox.setAttribute("type", "checkbox")
+    checkBox.classList.add("checkBox")
+
+    task.appendChild(checkBox)
+
+    checkBox.addEventListener("change", () => {
+        checkBox.classList.toggle("checked");
+    });
+
+// Remove botton
+    const remove = document.createElement("input")
+    remove.setAttribute("type", "button")
+    remove.setAttribute("value", "Remover")
+    remove.classList.add("removeBotton")
+
+    task.appendChild(remove)
+
+    remove.addEventListener("click", () => {
+        task.remove()
+    })
 }
 
 input.addEventListener("keydown", (e) => {
